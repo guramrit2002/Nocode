336470fc6f1f637eb4c8b196aa16b750ecfb4d67
guramrit <guramrit1066@gmail.com>
2025-12-25 15:46:18 +0530

    new -> json and status api

--- backend_nocode/backend_nocode/settings.py
+++ backend_nocode/backend_nocode/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'builder',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
--- backend_nocode/builder/views.py
+++ backend_nocode/builder/views.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status as status_code
+from rest_framework.decorators import action
 from django.shortcuts import render
 from .handlers import ProjectHandler
 from backend_nocode.utils import response
@@ -54,6 +55,17 @@ class ProjectViewSet(ViewSet):
             traceback.print_exc()
             return response("Something Went wrong",{"error": str(e)},
                             status=status_code.HTTP_500_INTERNAL_SERVER_ERROR)
+    @action(detail=True, methods=["get"])
+    def json_status(self,request,pk):
+        try:
+            obj = ProjectHandler()
+            message, date, status = obj.get_status_json(request,pk)
+            return response(message=message, data=date, status=status)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            return response("Something Went wrong",{"error": str(e)},
+                            status=status_code.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class ProjectPublishSaveViewSet(ViewSet):
     
@@ -70,3 +82,4 @@ class ProjectPublishSaveViewSet(ViewSet):
             import traceback
             traceback.print_exc()
             return response("Something Went wrong",{"error": str(e)})
+    
